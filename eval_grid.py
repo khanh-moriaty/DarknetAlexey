a3308ba000f67fa0f79d19acff2c4b27b134f8b3
moriaty <19520624@gm.uit.edu.vn>
2020-11-03 00:06:14 -0800

    Changed eval.py algorithm

--- eval_grid.py
+++ eval_grid.py
@@ -9,15 +9,14 @@ parser.add_argument('--pred', type=str,
 parser.add_argument('--gt', type=str, 
                     default="data/ocr/test/",
                     help="Path to ground truth directory, which should contain all .txt label files.")
-parser.add_argument('--thresh', type=float, 
-                    default=0.1,
-                    help="Maximum distance between predicted and gt. Value in range 0 to 1, scaled by cmtnd's size. (Default: 0.1)")
+# parser.add_argument('--ratio', type=float, 
+#                     default="data/test/",
+#                     help="Ratio used f")
 
 args = parser.parse_args()
 
 PRED_DIR = args.pred
 GT_DIR = args.gt
-THRESHOLD = args.thresh
 
 pred_dir = os.listdir(PRED_DIR)
 gt_dir = os.listdir(GT_DIR)
@@ -29,61 +28,64 @@ TN = [0] * 5
 FN = [0] * 5
 
 for fi in gt_dir:
-    # input()
+    # print(fi)
     gt_path = os.path.join(GT_DIR, fi)
     pred_path = os.path.join(PRED_DIR, fi)
     if not os.path.isfile(pred_path): 
         print('No matching prediction file found: ', fi)
         continue
     
-    found = [False] * 4
-    positive = [False] * 4
-    
-    gt_pts = [(-1, -1)] * 4
-    xmax = -1
-    xmin = 10**9
-    ymax = -1
-    ymin = 10**9
+    bbox = []
+    check = [True] * 4
     with open(gt_path, 'r') as f:
         lines = f.read().splitlines()
     for line in lines:
         content = line.split()
         name = int(content[0])
         if name not in range(4): continue
-        found[name] = True
-        positive[name] = True
+        check[name] = False
         x, y, w, h = [float(x) for x in content[1:]]
-        gt_pts[name] = (x, y)
-        xmax = max(x, xmax)
-        xmin = min(x, xmin)
-        ymax = max(y, ymax)
-        ymin = min(y, ymin)
-        
-    w = xmax - xmin if xmax != -1 and xmin != 10**9 else -1
-    h = ymax - ymin if ymax != -1 and ymin != 10**9 else w
-    w = w if w != -1 else h
-    size = w ** 2 + h ** 2 if w != -1 else 2
+        bbox += [
+            [x-w/2, y-h/2],
+            [x+w/2, y-h/2],
+            [x+w/2, y+h/2],
+            [x-w/2, y+h/2],
+        ]
+    xmin = min(bbox, key=lambda x: x[0], default=[0,0])[0]
+    xmax = max(bbox, key=lambda x: x[0], default=[1,1])[0]
+    ymin = min(bbox, key=lambda x: x[1], default=[0,0])[1]
+    ymax = max(bbox, key=lambda x: x[1], default=[1,1])[1]
+    
+    def get_gt(x, y):
+        if ymin <= y <= ymin + (ymax - ymin) / 3: 
+            if xmin <= x <= xmin + (xmax - xmin) / 3: 
+                return 0 # topleft
+            if xmin + (xmax - xmin) / 3 <= x <= xmax: 
+                return 1 # topright
+        if ymax - (ymax - ymin) / 3 <= y <= ymax: 
+            if xmin <= x <= xmin + (xmax - xmin) / 3: 
+                return 2 # botleft
+            if xmin + (xmax - xmin) / 3 <= x <= xmax: 
+                return 3 # botright
+        return -1 # not valid
+    
     with open(pred_path, 'r') as f:
         lines = f.read().splitlines()
     for line in lines:
         content = line.split()
         name = int(content[0])
         assert name in range(4), pred_path + " ERROR: invalid class."
-        x, y = gt_pts[name]
-        if x == -1:
-            FP[name] += 1
-            continue
-        
-        found[name] = False
-        dist = (x - float(content[1])) ** 2 + (y - float(content[2])) ** 2
-        if dist <= size * THRESHOLD ** 2:
-            positive[name] = False
+        check[name] = True
+        x, y, w, h = [float(x) for x in content[1:]]
+        gt = get_gt(x, y)
+        if name == gt:
             TP[name] += 1
         else:
             FP[name] += 1
-        
-    for i, x in enumerate(found):
-        if x:
+            FN[gt] += 1
+    
+    for i, x in enumerate(check):
+        if not x:
             FN[i] += 1
         
 precision = [tp/(tp+fp) if tp+fp > 0 else 0 for tp, fp in zip(TP, FP)]
